ae8a6e71623a79e4cd98fe015e1be030f493defd
pongdone2 <161256880+pongdone2@users.noreply.github.com>
2024-10-30 18:07:55 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 //Instagram: techgod143
 //Telegram: t.me/techgod143
 //GitHub: @techgod143
-//WhatsApp: +917466008456
+//WhatsApp: +23278188655
 //want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@techgod143
 
 const {
